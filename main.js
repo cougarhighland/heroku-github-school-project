89feb89d812876ae3c708a11a66d93bc862ac9ce
xuan tran <xuandundkl@gmail.com>
2022-03-24 17:32:54 +0100

    fix directory error

--- main.js
+++ main.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 1337;
 
-app.use(express.static('client'));
+
 
 //omitted express.json() to show difference
 
 app.use(express.json());
+app.use(express.static('./client'));
 
 app.listen(PORT, () =>{
     console.log('server is running on port 1337');
